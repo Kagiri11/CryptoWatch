d72c83303299e27f46be28afde4bed2183cbe485
Kagiri11 <charlesmaish423@gmail.com>
2021-07-09 11:58:42 +0300

    first commit

---
+++ app/src/main/java/com/example/cryptowatch/models/Coins.kt
@@ -0,0 +1,3 @@
+package com.example.cryptowatch.models
+
+class Coins : ArrayList<CoinItem>()
---
+++ app/src/main/java/com/example/cryptowatch/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cryptowatch.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cryptowatch.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
